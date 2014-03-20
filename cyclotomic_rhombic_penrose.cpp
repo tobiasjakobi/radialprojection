7a0eb456e7b3613270c5b530cd45bbcd542bb97d
Tobias Jakobi <tjakobi@math.uni-bielefeld.de>
2014-03-20 12:21:10 +0100

    Add GPLv3 header to source files and headers

--- cyclotomic_rhombic_penrose.cpp
+++ cyclotomic_rhombic_penrose.cpp
@@ -1,3 +1,18 @@
+/*  radialprojection - tools to numerically compute the radial projection of point sets
+ *  Copyright (C) 2012-2014 - Tobias Jakobi <tjakobi at math dot uni dash bielefeld dot de>
+ *
+ *  radialprojection is free software: you can redistribute it and/or modify it under the terms
+ *  of the GNU General Public License as published by the Free Software Foundation, either
+ *  version 3 of the License, or (at your option) any later version.
+ *
+ *  radialprojection is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ *  PURPOSE. See the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along with radialprojection.
+ *  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "cyclotomic_rhombic_penrose.h"
 
 #include "level_manager.h"
