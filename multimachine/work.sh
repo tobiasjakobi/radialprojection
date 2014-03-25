406b87e912788b7c84d17e4f2da21a8b4fc48acb
Tobias Jakobi <tjakobi@math.uni-bielefeld.de>
2014-03-25 18:59:55 +0100

    multimachine: move fetch code around

--- multimachine/work.sh
+++ multimachine/work.sh
@@ -6,17 +6,6 @@ export PATH=$(pwd):$PATH
 
 source "mmachine_scripts"
 
-if [ -z $1 ]; then
-  echo "status: fetching patch data..."
-  mmachine_fetchdata
-  if [ $? -ne 0 ]; then
-    echo "error: fetching patch data failed, aborting..."
-    exit 1
-  else
-    echo "status: patch data successfully fetched"
-  fi
-fi
-
 mmachine_slave_main $*
 if [ $? -ne 0 ]; then
   echo "error: slave main routine failed"
