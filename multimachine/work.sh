2d6ad681a527de6801340e70243a0588c970506d
Tobias Jakobi <tjakobi@math.uni-bielefeld.de>
2014-03-25 14:02:52 +0100

    multimachine: use /vol/tmp instead of the homes directory
    Also add more error handling, and error output to make
    diagonostics easier.

--- multimachine/work.sh
+++ multimachine/work.sh
@@ -6,7 +6,21 @@ export PATH=$(pwd):$PATH
 
 source "mmachine_scripts"
 
+echo "status: fetching patch data..."
 mmachine_fetchdata
+if [ $? -ne 0 ]; then
+  echo "error: fetching patch data failed, aborting..."
+  exit 1
+else
+  echo "status: patch data successfully fetched"
+fi
+
 mmachine_slave_main $*
+if [ $? -ne 0 ]; then
+  echo "error: slave main routine failed"
+  exit 1
+else
+  echo "status: slave main routine successfully called"
+fi
 
 popd > /dev/null
