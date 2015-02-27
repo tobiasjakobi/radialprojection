a041fab70ea645de2faa2cfdf8812f7156bfde03
Tobias Jakobi <tjakobi@math.uni-bielefeld.de>
2015-02-27 15:44:02 +0100

    cyclotomic_octagonal: use extractVisible in projTilingVis

--- src/cyclotomic_octagonal.cpp
+++ src/cyclotomic_octagonal.cpp
@@ -167,30 +167,7 @@ void Octagonal::projTilingVis(const vec4i& initpoint,
   cerr << "Constructed patch of octagonal tiling with "
        << tilingpoints.size() << " vertices.\n";
 
-  // We're not removing vertices in this case, so allocate the full amount.
-  VisList* vlist = new VisList;
-  vlist->reserve(tilingpoints.size() - 1);
-
-  vlist->init();
-
-  for (vec4ilist::const_iterator i = tilingpoints.begin(); i != tilingpoints.end(); ++i) {
-    const vec4i shifted(*i - origin);
-
-    if (shifted.isZero()) continue;
-    vlist->insertSorted(shifted);
-  }
-
-  if (radialproj)
-    vlist->removeInvisibleFast();
-  else
-    vlist->removeInvisibleProper();
-
-  visiblepoints.clear();
-  visiblepoints.reserve(vlist->size());
-  vlist->dump(visiblepoints);
-
-  delete vlist;
-  vlist = NULL;
+  extractVisible(origin, radialproj, tilingpoints, visiblepoints);
 }
 
 // See projTilingVis for more comments on the code.
@@ -254,6 +231,7 @@ void Octagonal::extractVisible(const vec4i& origin, bool radialproj,
                           Common::vec4ilist& output) {
   using namespace Common;
 
+  // We're not removing vertices in this case, so allocate the full amount.
   VisList* vlist = new VisList;
   vlist->reserve(input.size() - 1);
 
