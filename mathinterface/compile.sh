6d14266aee51bb52af0bd3139e36567f4edfea02
Tobias Jakobi <tjakobi@math.uni-bielefeld.de>
2016-09-02 17:49:52 +0200

    mathinterface: document path
    Path and library name depends on the Mathematica version used.

--- mathinterface/compile.sh
+++ mathinterface/compile.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
+## Adjust the directory depending on which Mathematica
+## version you target.
 mlinkdir="/var/tmp/Mathematica/9.0.1/SystemFiles/Links/MathLink/DeveloperKit"
+#mlinkdir="/var/tmp/Mathematica/10.3/SystemFiles/Links/MathLink/DeveloperKit"
+#mlinkdir="/var/tmp/Mathematica/11.0/SystemFiles/Links/MathLink/DeveloperKit"
+
 mlinkfiles="${mlinkdir}/Linux-x86-64/CompilerAdditions"
 mprep="${mlinkfiles}/mprep"
 
 includes="${mlinkfiles}"
 libdir="${mlinkfiles}"
+
+## Mathematica9 still uses libML64i3, while later
+## versions use the i4 version.
 libraries="-lML64i3 -lm -lpthread -lrt -lstdc++"
+#libraries="-lML64i4 -lm -lpthread -lrt -lstdc++"
 
 # clean
 rm -f minterface.c minterface.o feed2mathematica.o feed2mathematica
@@ -18,4 +27,3 @@ gcc -c -I"${includes}" minterface.c
 g++ -c -I"${includes}" feed2mathematica.cpp
 
 g++ feed2mathematica.o minterface.o -L"${libdir}" $libraries -o feed2mathematica
-
